2e6a16e1229bdb78b40a9fedbf39aa94cb80ed19
serejja <serejja@gmail.com>
2014-01-15 06:53:05 +0200

    track number fix, try to parse as int

--- app/models/Mp3Handler.scala
+++ app/models/Mp3Handler.scala
@@ -10,7 +10,7 @@ object Mp3Handler extends FormatHandler {
 	  val name = mp3File.getId3v2Tag().getTitle
 	  val duration = Tracks.parseDuration(mp3File.getLengthInSeconds())
 	  val location = destination + file.getName
-	  val trackNo = mp3File.getId3v2Tag().getTrack().toInt
+	  val trackNo = mp3File.getId3v2Tag().getTrack().toCharArray().filter(_.isDigit).mkString.toInt
 	  val albumid = album.id
 	  if (!file.renameTo(new File(location))) {
 	    Log.warn("Path " + location + " already exists. Ignoring file")
